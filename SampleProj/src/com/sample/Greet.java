27a61fa3d5080e236f5d09319d636bda2a76b7b0
sanket-1999 <60914794+sanket-1999@users.noreply.github.com>
2020-04-12 20:14:19 +0530

    hibernate first

--- SampleProj/src/com/sample/Greet.java
+++
@@ -1,7 +0,0 @@
-package com.sample;
-
-public class Greet {
-	public String greetMe(String name) {
-		return "Hello " + name;
-	}
-}
--- SampleProj/src/com/sample/GreetTest.java
+++
@@ -1,7 +0,0 @@
-package com.sample;
-
-public class GreetTest {
-	public static void main(String args[]) {
-		System.out.println(new Greet().greetMe("Anand"));
-	}
-}
